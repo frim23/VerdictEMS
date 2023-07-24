c713ecbe817a425ff6dabd305b847a4deebbb43e
frim23 <dima.kurua@mail.ru>
2023-07-24 20:10:04 +0400

    fax-revive import option, ND_Character var bootup fix, debug tool

--- client/client.lua
+++ client/client.lua
@@ -1,8 +1,16 @@
 local department = "LSFD"
 local fax_revive = true
+local needsImport = false
+local debug = false
 
+Citizen.Wait(1000)
 NDCore = exports["ND_Core"]:GetCoreObject()
 local character = NDCore.Functions.GetSelectedCharacter()
+
+RegisterNetEvent("ND:setCharacter")
+AddEventHandler("ND:setCharacter", function(newCharacter)
+    character = newCharacter
+end)
 
 RegisterNetEvent("VerdictEMS:healme")
 RegisterNetEvent('VerdictEMS:healsuccess')
@@ -26,21 +34,22 @@ AddEventHandler("VerdictEMS:healme",function()
 		  args = {"VerdictEMS", "You got cured by an EMT"}
 	  })
 end)
-AddEventHandler("VerdictEMS:reviveme",function()
-		if GetEntityHealth(PlayerPedId()) < 110 then
-			if fax_revive then
-    		exports["fax-revive"]:revivePed(PlayerPedId())
-    		print("fax revive")
-    	else
-    		SetEntityHealth(PlayerPedId(), 110)
-    		print("no fax revive")
-    	end
-    	TriggerEvent('chat:addMessage', {
-			  color = { 44, 87, 242},
-			  multiline = true,
-			  args = {"VerdictEMS", "You got revived by an EMT"}
-		  })
+AddEventHandler("VerdictEMS:reviveme", function()
+    if GetEntityHealth(PlayerPedId()) < 110 then
+        if fax_revive and needsImport then
+            exports["fax-revive"]:revivePed(PlayerPedId())
+        elseif fax_revive and not needsImport then
+            revivePed(PlayerPedId())
+        else
+            SetEntityHealth(PlayerPedId(), 110)
+        end
+        TriggerEvent('chat:addMessage', {
+            color = { 44, 87, 242},
+            multiline = true,
+            args = {"VerdictEMS", "You got revived by an EMT"}
+        })
     else
+        -- Player's health is above 110
     end
 end)
 
@@ -76,7 +85,7 @@ RegisterCommand('revive', function(_, args)
 				TriggerEvent('chat:addMessage', {
 				  color = { 44, 87, 242},
 				  multiline = true,
-				  args = {"VerdictEMS", "You are not a medical personnel"}
+				  args = {"VerdictEMS", "You are not a medical personnel" .. character.job}
 			  })
 			end
 		end
@@ -109,6 +118,12 @@ RegisterCommand('heal', function(_, args)
 		print("No character selected")
 	end
 end)
+
+if debug then
+	RegisterCommand('revivedebug', function(_, args)
+		SetEntityHealth(PlayerPedId(), 101)
+	end)
+end
 
 TriggerEvent('chat:addSuggestion', '/revive', 'Revive an injured player (Available to SAFR/EMT only)', {
     { name="Player ID", help="Player's server ID" }
